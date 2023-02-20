6e319d2a6596132a88c6cf0822f16903133b1361
Jonathan Reyes <jonathan.reyes@grupor5.com>
2023-02-19 20:03:47 -0500

    chore: config docker with postgres to run in machine local

--- nativ_app_test/settings.py
+++ nativ_app_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3=#_&ha@45u66wnh$e55_4iip6ab%#$z1uimbs(y3k!uio==0&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'postgres']
 
 
 # Application definition
