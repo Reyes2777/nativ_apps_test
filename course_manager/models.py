2697cda63d1629b74baa12bf712725d492b9ac8f
Jonathan Reyes <jonathan.reyes@grupor5.com>
2023-02-19 23:02:22 -0500

    feat: create app in django to course manager

---
+++ course_manager/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    schedule = models.CharField(max_length=50)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    email = models.EmailField()
+    courses = models.ManyToManyField('Course')
+
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
